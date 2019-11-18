8e3cbe464433dffda1bddb9e946beb3c39cd7362
liuchang56789 <1551015601@qq.com>
2019-11-18 16:28:52 +0800

    botombar

--- Project/CatchTime/app/src/main/java/com/example/catchtime/MainActivity.java
+++ Project/CatchTime/app/src/main/java/com/example/catchtime/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.catchtime;
 
+import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (getSupportActionBar() != null){
+            getSupportActionBar().hide();
+        }
+        BottomBar bottomBar=findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+//                if (tabId == R.id.tab_favorites) {
+//                    // The tab with id R.id.tab_favorites was selected,
+//                    // change your content accordingly.
+//                }
+            }
+        });
     }
+
 }
