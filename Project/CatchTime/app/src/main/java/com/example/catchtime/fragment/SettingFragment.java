fd6bdd678a70a7b1b8a5c2010a8faafc26eee1c8
liuguosheng1 <1806479835@qq.com>
2019-11-22 22:41:54 +0800

    ll

--- Project/CatchTime/app/src/main/java/com/example/catchtime/fragment/SettingFragment.java
+++ Project/CatchTime/app/src/main/java/com/example/catchtime/fragment/SettingFragment.java
@@ -1,10 +1,12 @@
 package com.example.catchtime.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.catchtime.R;
 
@@ -13,12 +15,50 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class SettingFragment extends Fragment {
+    private ImageView userImg;
+    private ImageView activityImg;
+    private ImageView settingImg;
+    private ImageView helpImg;
+    private customListener listener;
+    private View view;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        return super.onCreateView(inflater, container, savedInstanceState);
-        Log.e("test","初始化第3个页面");
-        View view=inflater.inflate(R.layout.settingfragment,null);
+        view=inflater.inflate(R.layout.settingfragment,null);
+        getViews();
+        onLisener();
         return view;
+    }
+
+    private void onLisener() {
+        userImg.setOnClickListener(listener);
+        activityImg.setOnClickListener(listener);
+        settingImg.setOnClickListener(listener);
+        helpImg.setOnClickListener(listener);
+    }
+
+    private void getViews() {
+        userImg=view.findViewById(R.id.uesr);
+        activityImg = view.findViewById(R.id.add_activity);
+        settingImg = view.findViewById(R.id.setting);
+        helpImg = view.findViewById(R.id.help);
+    }
+
+    class customListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.uesr:
+                    break;
+                case R.id.add_activity:
+                    break;
+                case R.id.setting:
+                    break;
+                case R.id.help:
+                    break;
+            }
+        }
     }
 }
