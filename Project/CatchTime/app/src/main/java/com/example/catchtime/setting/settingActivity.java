5d6743e5f74b7d2c3482432d0b17640ebc651954
liuguosheng1 <1806479835@qq.com>
2019-11-23 17:11:35 +0800

    l

--- Project/CatchTime/app/src/main/java/com/example/catchtime/setting/settingActivity.java
+++ Project/CatchTime/app/src/main/java/com/example/catchtime/setting/settingActivity.java
@@ -1,20 +1,16 @@
 package com.example.catchtime.setting;
 
 import androidx.appcompat.app.AppCompatActivity;
-import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
 
 import android.os.Bundle;
 
 import com.example.catchtime.R;
 
-public class user extends SwipeBackActivity {
+public class settingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user);
-        if (getSupportActionBar() != null){
-            getSupportActionBar().hide();
-        }
+        setContentView(R.layout.activity_setting);
     }
 }
