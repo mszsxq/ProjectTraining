09a7ff894a7fd66d466059f257835d75d9d65073
mszsxq <493906387@qq.com>
2019-11-25 17:14:06 +0800

    details

--- Project/CatchTime/app/src/main/java/com/example/catchtime/activity/ActivitiesDetail.java
+++ Project/CatchTime/app/src/main/java/com/example/catchtime/activity/ActivitiesDetail.java
@@ -1,5 +1,7 @@
 package com.example.catchtime.activity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,12 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
+import com.example.catchtime.MainActivity;
 import com.example.catchtime.R;
 import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
 import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
@@ -37,20 +41,29 @@ public class ActivitiesDetail extends SwipeBackActivity implements ObservableScr
     private ProgressBar progesss;
     private TextView min;
     private RelativeLayout view_dayoccupy;
-
+    private  ImageView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activities_detail);
+        getView();
+        progesss.setProgress(20);
+        min.setText(new StringBuffer().append(progesss.getProgress()).append("min"));
+        //进度条
+//        setPosWay1();
+        setListener();
+
+
+    }
+
+    public void getView() {
+        back = (ImageView) findViewById(R.id.acdetails_back);
         view_dayoccupy = (RelativeLayout) findViewById(R.id.view_dayoccupy1);
         progesss = (ProgressBar) findViewById(R.id.progesss1);
         min = (TextView) findViewById(R.id.min1);
-
-        progesss.setProgress(66);
-        min.setText(new StringBuffer().append(progesss.getProgress()).append("min"));
-        setPosWay1();
-
-
+    }
+    public void setListener(){
+        back.setOnClickListener(new MyListener());
     }
 
     @Override
@@ -65,42 +78,63 @@ public class ActivitiesDetail extends SwipeBackActivity implements ObservableScr
 
     @Override
     public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.topbar);
+        if(linearLayout==null){
+            return ;
+        }
+        if(scrollState == ScrollState.UP){
+            ViewGroup.LayoutParams lp;
+            lp= linearLayout.getLayoutParams();
+            lp.height=200;
+            linearLayout.setLayoutParams(lp);
 
+        }
     }
-    //进度条
-    private void setPosWay1() {
-       min.post(new Runnable() {
-            @Override
-            public void run() {
-                setPos();
+//    //进度条
+//    private void setPosWay1() {
+//       min.post(new Runnable() {
+//            @Override
+//            public void run() {
+//                setPos();
+//            }
+//        });
+//    }
+//    @Override
+//    public void onWindowFocusChanged(boolean hasFocus) {
+//        super.onWindowFocusChanged(hasFocus);
+//        if (hasFocus) {
+//            setPos();
+//        }
+//    }
+//    /**
+//     * 设置进度显示在对应的位置
+//     */
+//    public void setPos() {
+//        int w = getWindowManager().getDefaultDisplay().getWidth();
+//        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) min.getLayoutParams();
+//        int pro = progesss.getProgress();
+//        int tW = min.getWidth();
+//        if (w * pro / 100 + tW * 0.3 > w) {
+//            params.leftMargin = (int) (w - tW * 1.1);
+//        } else if (w * pro / 100 < tW * 0.7) {
+//            params.leftMargin = 0;
+//        } else {
+//            params.leftMargin = (int) (w * pro / 100 - tW * 0.7);
+//        }
+//        min.setLayoutParams(params);
+//
+//    }
+    class MyListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.acdetails_back :
+                    finish();
+                    break;
+
             }
-        });
-    }
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-            setPos();
         }
     }
-    /**
-     * 设置进度显示在对应的位置
-     */
-    public void setPos() {
-        int w = getWindowManager().getDefaultDisplay().getWidth();
-        Log.e("w=====", "" + w);
-        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) min.getLayoutParams();
-        int pro = progesss.getProgress();
-        int tW = min.getWidth();
-        if (w * pro / 100 + tW * 0.3 > w) {
-            params.leftMargin = (int) (w - tW * 1.1);
-        } else if (w * pro / 100 < tW * 0.7) {
-            params.leftMargin = 0;
-        } else {
-            params.leftMargin = (int) (w * pro / 100 - tW * 0.7);
-        }
-        min.setLayoutParams(params);
-
-    }
-
 }
+
