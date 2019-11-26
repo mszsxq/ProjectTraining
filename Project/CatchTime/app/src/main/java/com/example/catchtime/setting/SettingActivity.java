d7cf7a0fe715e6a9603788d9c864b09fa25bdcf7
yanmanhuan <1807355098@qq.com>
2019-11-26 15:31:09 +0800

    ym

--- Project/CatchTime/app/src/main/java/com/example/catchtime/setting/SettingActivity.java
+++ Project/CatchTime/app/src/main/java/com/example/catchtime/setting/SettingActivity.java
@@ -11,6 +11,6 @@ public class SettingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
+        setContentView(R.layout.login);
     }
 }
