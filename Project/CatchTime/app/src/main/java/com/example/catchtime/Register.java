f788f302e09d632ebf46edfeddf068c4f5517583
mamengyuan7 <3023923835@qq.com>
2019-11-30 18:19:28 +0800

    m

--- Project/CatchTime/app/src/main/java/com/example/catchtime/Register.java
+++ Project/CatchTime/app/src/main/java/com/example/catchtime/Register.java
@@ -1,18 +1,28 @@
 package com.example.catchtime;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.text.method.TransformationMethod;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import cn.bmob.sms.BmobSMS;
+import cn.bmob.sms.exception.BmobException;
+import cn.bmob.sms.listener.RequestSMSCodeListener;
+import cn.bmob.sms.listener.VerifySMSCodeListener;
+
 public class Register extends AppCompatActivity {
     private TextView btn_login;
     private TextView btn_register;
@@ -20,12 +30,16 @@ public class Register extends AppCompatActivity {
     private TextView full;
     private ImageView eyes1;
     private EditText user_pwd1;
+    private Button countdown;
+    private Button register;
+    private EditText et;
     //默认密码输入框为隐藏的
     private boolean isHideFirst = true;
     private CustomOnclickListner listner;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+        BmobSMS.initialize(Register.this, "c6cdff9c3ade26719c30c17eb8f38d4b");
         getviews();
         registers();
         // 监听号码输入框的字数
@@ -59,11 +73,16 @@ public class Register extends AppCompatActivity {
         full=findViewById(R.id.full);
         user_pwd1=findViewById(R.id.user_pwd1);
         eyes1=findViewById(R.id.eyes1);
+        register = findViewById(R.id.re_btn_register);
+        countdown = findViewById(R.id.re_btn_countdown);
+        et = findViewById(R.id.re_et);
     }
     private void registers() {
         listner=new CustomOnclickListner();
         btn_login.setOnClickListener(listner);
         eyes1.setOnClickListener(listner);
+        register.setOnClickListener(listner);
+        countdown.setOnClickListener(listner);
     }
 
     class CustomOnclickListner implements View.OnClickListener{
@@ -76,6 +95,43 @@ public class Register extends AppCompatActivity {
                     startActivity(intent);
                     overridePendingTransition(R.anim.in,R.anim.out);
                     finish();
+                    break;
+                case R.id.re_btn_countdown:
+                    // 将按钮设置为不可用状态
+                    countdown.setEnabled(false);
+                    // 启动倒计时的服务
+                    new CountDownTimer(60000, 1000) {
+                        @SuppressLint("ResourceAsColor")
+                        @Override
+                        public void onTick(long millisUntilFinished) {
+                            countdown.setBackgroundResource(R.color.orange_dark);
+                            countdown.setTextColor(R.color.white);
+                            countdown.setText(millisUntilFinished / 1000 + "秒");
+                        }
+
+                        @SuppressLint("ResourceAsColor")
+                        @Override
+                        public void onFinish() {
+                            countdown.setClickable(true);
+                            countdown.setBackgroundResource(R.color.gray);
+                            countdown.setTextColor(R.color.orange_dark);
+                            countdown.setText("重新发送");
+                            countdown.setEnabled(true);
+                        }
+                    }.start();
+                    Log.e("MESSAGE:", "4");
+                    String phone = full_re.getText().toString();
+                    Log.e("mmy",phone);
+                    BmobSMS.requestSMSCode(Register.this,phone, "注册验证码", new RequestSMSCodeListener() {
+                        @Override
+                        public void done(Integer smsId, BmobException ex) {
+                            if (ex == null) {//验证码发送成功
+                                Log.e("bmob", "短信id：" + smsId);//用于查询本次短信发送详情
+                            }else {
+                                Log.e("bmob","errorCode = "+ex.getErrorCode()+",errorMsg = "+ex.getLocalizedMessage());
+                            }
+                        }
+                    });
                     break;
                 case R.id.eyes1:
                     if(isHideFirst==true){
@@ -93,7 +149,27 @@ public class Register extends AppCompatActivity {
                     int index=user_pwd1.getText().toString().length();
                     user_pwd1.setSelection(index);
                     break;
-            }
+                case R.id.re_btn_register:
+                    String number = et.getText().toString();
+                    if (!TextUtils.isEmpty(number)) {
+                        BmobSMS.verifySmsCode(Register.this, full_re.getText().toString(), number, new VerifySMSCodeListener() {
+                            @Override
+                            public void done(BmobException ex) {
+                                if (ex == null) {//短信验证码已验证成功
+                                    Log.e("bmob", "验证通过");
+                                    Intent intent=new Intent();
+                                    intent.setClass(Register.this, Login.class);
+                                    startActivity(intent);
+                                    overridePendingTransition(R.anim.in,R.anim.out);
+                                    finish();
+                                } else {
+                                    Log.e("bmob", "验证失败：code =" + ex.getErrorCode() + ",msg = " + ex.getLocalizedMessage());
+                                }
+                            }
+                        });
+                    }
+                    break;
             }
         }
+    }
 }
