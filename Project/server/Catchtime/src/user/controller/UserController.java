a47eca81c486b2cb265e2e69c607044983da3cb2
mamengyuan7 <3023923835@qq.com>
2019-11-30 20:24:10 +0800

    m

--- Project/server/Catchtime/src/user/controller/UserController.java
+++ Project/server/Catchtime/src/user/controller/UserController.java
@@ -1,6 +1,8 @@
 package user.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Type;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -10,8 +12,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import entity.User;
 import user.dao.UserDao;
+import user.service.UserService;
 
 /**
  * Servlet implementation class UserController
@@ -34,6 +40,21 @@ public class UserController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		String client = request.getParameter("client");
+		System.out.println(client);
+		if(client != null) {
+			Type listType=new TypeToken<User>(){}.getType();
+			Gson gson=new Gson();
+	        User user = gson.fromJson(client,listType);
+	        UserService userService = new UserService();
+	        int n = userService.addUser(user.getPhone(),user.getPassword());
+	        if(n!=0) {
+				out.write("注册成功");
+			}else {
+				out.write("手机号码重复使用!");
+			}
+		}
 	}
 
 	/**
