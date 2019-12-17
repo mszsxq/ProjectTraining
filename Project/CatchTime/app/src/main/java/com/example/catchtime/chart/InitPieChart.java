c78e5910fd668580b6aecd7d77464de381756a79
mamengyuan7 <3023923835@qq.com>
2019-12-17 10:26:39 +0800

    mmy

--- Project/CatchTime/app/src/main/java/com/example/catchtime/chart/InitPieChart.java
+++ Project/CatchTime/app/src/main/java/com/example/catchtime/chart/InitPieChart.java
@@ -132,7 +132,7 @@ public class InitPieChart  extends DemoBase implements OnChartValueSelectedListe
         // entry label styling
         chart.setEntryLabelColor(Color.WHITE);
         chart.setEntryLabelTypeface(tfRegular);
-        chart.setEntryLabelTextSize(12f);
+        chart.setEntryLabelTextSize(0);
 
         setData(entries);//设置数据函数
 
@@ -147,33 +147,13 @@ public class InitPieChart  extends DemoBase implements OnChartValueSelectedListe
         dataSet.setIconsOffset(new MPPointF(1, 3));//设置图标显示的位置
         dataSet.setSelectionShift(8f);//设置点击时突出的部分大小
 
-        // add a lot of colors
-
-//        ArrayList<Integer> colors = new ArrayList<>();
-//
-//        for (int c : ColorTemplate.VORDIPLOM_COLORS)
-//            colors.add(c);
-//
-//        for (int c : ColorTemplate.JOYFUL_COLORS)
-//            colors.add(c);
-//
-//        for (int c : ColorTemplate.COLORFUL_COLORS)
-//            colors.add(c);
-//
-//        for (int c : ColorTemplate.LIBERTY_COLORS)
-//            colors.add(c);
-//
-//        for (int c : ColorTemplate.PASTEL_COLORS)
-//            colors.add(c);
-
-//        colors.add(ColorTemplate.getHoloBlue());
 
         dataSet.setColors(colors);
         //dataSet.setSelectionShift(0f);
 
         PieData data = new PieData(dataSet);
         data.setValueFormatter(new PercentFormatter(chart));
-        data.setValueTextSize(15f);
+        data.setValueTextSize(0);
         data.setValueTextColor(Color.WHITE);
         data.setValueTypeface(tfLight);
         chart.setData(data);
@@ -209,6 +189,9 @@ public class InitPieChart  extends DemoBase implements OnChartValueSelectedListe
         Log.i("VAL SELECTED",
                 "Value: " + e.getY() + ", index: " + h.getX()
                         + ", DataSet index: " + h.getDataSetIndex());
+        String value = entries.get((int) h.getX()).getLabel();
+        String[] list = value.split("/");
+        chart.setCenterText(list[0]+"\n"+list[1]);
         if(type==0) {
             linearLayout.setVisibility(View.VISIBLE);
             GradientDrawable drawable = (GradientDrawable) add.getBackground();
@@ -270,8 +253,8 @@ public class InitPieChart  extends DemoBase implements OnChartValueSelectedListe
                         String value = entries.get((int) h.getX()).getLabel();
                         String[] list = value.split("/");
                         intent.putExtra("activity_name", list[0]);
-                        intent.putExtra("color",colors.get(h.getDataSetIndex()).intValue());
-                        intent.putExtra("icon", String.valueOf(entries.get(h.getDataSetIndex()).getIcon()));
+                        intent.putExtra("color",colors.get((int) h.getX()).intValue());
+                        intent.putExtra("icon", String.valueOf(entries.get((int) h.getX()).getIcon()));
                         context.startActivity(intent);
                     }
                 });
